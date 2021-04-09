62bf9d925b10ad18d8a71d24b4cff04d92269944
fahmi-16 <hafizhol2001@gmail.com>
2021-04-09 21:05:44 +0800

    makai switch jarr...

---
+++ src/operator/Switch.java
@@ -0,0 +1,24 @@
+package operator;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanjalan=3;
+        switch (jalanjalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case 2:
+                System.out.println("urang acara");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+                break;
+            default:
+                System.out.println("makhluk halus");
+                break;
+        }
+    }
+}
