f9a86a43af788e3b5fffb29c9eedf574f5dad883
fahmi-16 <hafizhol2001@gmail.com>
2021-04-09 20:18:17 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak di jalankan program
+
+    String nama = "Hafizhol Fahmi";
+    int jumlahSks = 24;
+    double inddeksPrestasiKumulatif = 3.89;
+
+
+}
